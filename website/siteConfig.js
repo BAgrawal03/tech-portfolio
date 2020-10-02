b64ef45c1e2c074a81e7b3e008247192f9b1fbf2
Bhumika Agrawal <agr.bhumika03@gmail.com>
2020-10-02 15:17:40 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Bhumika Agrawal', // Title for your website.
     tagline: 'Continuously improving, aspiring Data Scientist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://bagrawal.github.io', // Your website URL
+    url: 'https://bagrawal03.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
